4d5b896ecb074c6cb712b81ea3efd11deb0d4ca9
Amol Gawai <5114143+amolgawai@users.noreply.github.com>
2017-12-27 21:44:07 +0100

    window navigation - ace-window
    window navigation settings.
    added ace-window

---
+++ windowNav.el
@@ -0,0 +1,40 @@
+;;; windowNav.el --- window navigation configurations  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2017  Amol Gawai
+
+;; Author: Amol Gawai <amol@doesnot.exist>
+;; Keywords: lisp, abbrev
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; navigating windows with packages and settings
+
+;;; Code:
+
+(use-package ace-window
+  :ensure t
+  :init
+  (progn
+    (setq aw-scope 'frame)
+    (global-set-key (kbd "C-x O") 'other-frame)
+    (global-set-key [remap other-window] 'ace-window)
+    (custom-set-faces
+     '(aw-leading-char-face
+       ((t (:inherit ace-jump-face-foreground :height 3.0)))))
+    ))
+
+(provide 'windowNav)
+;;; windowNav.el ends here
