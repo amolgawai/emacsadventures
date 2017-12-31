a45055a6f8d9b73011f2f3e52294fc0d19471bd9
Amol Gawai <5114143+amolgawai@users.noreply.github.com>
2017-12-31 23:09:45 +0100

    load all from directory
    the load-library seems not to work, so replaced with a simple function

--- loadMyConfig.el
+++ loadMyConfig.el
@@ -41,8 +41,18 @@
 (require 'pythonWithAnaconda)
 (require 'golang)
 
-(require 'load-directory)
-(load-directory (locate-user-emacs-file "emacsadventures/private"))
+;; Load private config
+(defun my-load-all-in-directory (dir)
+  "`load' all elisp libraries in directory DIR which are not already loaded."
+  (interactive "D")
+  (let ((libraries-loaded (mapcar #'file-name-sans-extension
+                                  (delq nil (mapcar #'car load-history)))))
+    (dolist (file (directory-files dir t ".+\\.elc?$"))
+      (let ((library (file-name-sans-extension file)))
+        (unless (member library libraries-loaded)
+          (load library nil t)
+          (push library libraries-loaded))))))
+(my-load-all-in-directory (locate-user-emacs-file "emacsadventures/private"))
 
 (provide 'loadMyConfig)
 ;;; loadMyConfig.el ends here
