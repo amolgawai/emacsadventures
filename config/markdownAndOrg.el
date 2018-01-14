ded9843abf33235b34dd0e3d3634c2c3856be4ac
Amol Gawai <5114143+amolgawai@users.noreply.github.com>
2018-01-14 23:03:28 +0100

    org mode first config

--- config/markdownAndOrg.el
+++ config/markdownAndOrg.el
@@ -45,7 +45,106 @@
   :mode ("\\.org\\'" . org-mode)
   :bind (("C-c l" . org-store-link)
 	 ("C-c a" . org-agenda)
-	 ("C-c b" . org-iswitchb)))
+	 ("C-c b" . org-iswitchb)
+         ("C-c c" . org-capture))
+  :config
+  (progn
+    (setq org-directory (expand-file-name "~/MyOrganiser"))
+    (setq org-default-notes-file (concat org-directory "/Inbox.org"))
+                                        ;    (setq org-agenda-files '("~/org" "~/www/org" "~/www/_org"))
+    (setq org-log-done t)
+    (setq org-fast-tag-selection-single-key t)
+    (setq org-use-fast-todo-selection t)
+                                        ;    (setq org-startup-truncated nil)
+    (setq org-todo-keywords
+          '(
+            (sequence "IDEA(i)" "TODO(t)" "STARTED(s)" "NEXT(n)" "WAITING(w)" "|" "DONE(d)")
+            (sequence "|" "CANCELED(c)" "DELEGATED(l)" "SOMEDAY(f)")
+            ))
+
+    (setq org-todo-keyword-faces
+          '(("IDEA" . (:foreground "GoldenRod" :weight bold))
+            ("NEXT" . (:foreground "IndianRed1" :weight bold))
+            ("STARTED" . (:foreground "OrangeRed" :weight bold))
+            ("WAITING" . (:foreground "coral" :weight bold))
+            ("CANCELED" . (:foreground "LimeGreen" :weight bold))
+            ("DELEGATED" . (:foreground "LimeGreen" :weight bold))
+            ("SOMEDAY" . (:foreground "LimeGreen" :weight bold))
+            ))
+
+    (setq org-tag-persistent-alist
+          '((:startgroup . nil)
+            ("HOME" . ?h)
+            ("RESEARCH" . ?r)
+            ("TEACHING" . ?t)
+            (:endgroup . nil)
+            (:startgroup . nil)
+            ("OS" . ?o)
+            ("DEV" . ?d)
+            ("WWW" . ?w)
+            (:endgroup . nil)
+            (:startgroup . nil)
+            ("EASY" . ?e)
+            ("MEDIUM" . ?m)
+            ("HARD" . ?a)
+            (:endgroup . nil)
+            ("URGENT" . ?u)
+            ("KEY" . ?k)
+            ("BONUS" . ?b)
+            ("noexport" . ?x)
+            )
+          )
+
+    (setq org-tag-faces
+          '(
+            ("HOME" . (:foreground "GoldenRod" :weight bold))
+            ("RESEARCH" . (:foreground "GoldenRod" :weight bold))
+            ("TEACHING" . (:foreground "GoldenRod" :weight bold))
+            ("OS" . (:foreground "IndianRed1" :weight bold))
+            ("DEV" . (:foreground "IndianRed1" :weight bold))
+            ("WWW" . (:foreground "IndianRed1" :weight bold))
+            ("URGENT" . (:foreground "Red" :weight bold))
+            ("KEY" . (:foreground "Red" :weight bold))
+            ("EASY" . (:foreground "OrangeRed" :weight bold))
+            ("MEDIUM" . (:foreground "OrangeRed" :weight bold))
+            ("HARD" . (:foreground "OrangeRed" :weight bold))
+            ("BONUS" . (:foreground "GoldenRod" :weight bold))
+            ("noexport" . (:foreground "LimeGreen" :weight bold))
+            ))
+    (setq org-agenda-ndays 7)
+    (setq org-agenda-show-all-dates t)
+    (setq org-agenda-skip-deadline-if-done t)
+    (setq org-agenda-skip-scheduled-if-done t)
+    (setq org-agenda-start-on-weekday nil)
+    (setq org-deadline-warning-days 14)
+    (setq org-agenda-custom-commands
+          '(("g" . "GTD contexts")
+            ("gh" "Home" tags-todo "HOME")
+            ("gu" "Urgent" tags-todo "URGENT")
+            ("G" "GTD Block Agenda"
+             ((todo "STARTED")
+              (tags-todo "URGENT")
+              (todo "NEXT"))
+             ((org-agenda-prefix-format "[ ] %T: ")
+              (org-agenda-with-colors nil)
+              (org-agenda-compact-blocks t)
+              (org-agenda-remove-tags t)
+              (ps-number-of-columns 2)
+              (ps-landscape-mode t))
+             ;;nil                      ;; i.e., no local settings
+             ("~/next-actions.txt"))
+            ))
+    ;; capture
+    (setq org-reverse-note-order t)
+
+    (setq org-capture-templates
+          '(("t" "Todo" entry (file+headline (concat org-directory "/Inbox.org") "Tasks")
+             "* TODO %?\nAdded: %U\n" :prepend t :kill-buffer t)
+            ("i" "Idea" entry (file+headline (concat org-directory "/Inbox.org") "Ideas")
+             "* IDEA %?\nAdded: %U\n" :prepend t :kill-buffer t)))
+    (setq org-use-speed-commands t)
+    (setq org-src-fontify-natively t)
+    (setq org-src-tab-acts-natively t)))
 
 (use-package org-bullets
   :ensure t
