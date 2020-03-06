4dd5e210b6148cb0bf319492c4beea390b428cfa
Amol Gawai <Amol.Gawai@doesnot.exist>
2020-03-06 10:05:49 +0100

    Revert "added ranger for directory navigation"
    This reverts commit a7ff42c82777a236007145810c0bf8104aed5778.

--- config/dirNav.el
+++ config/dirNav.el
@@ -142,9 +142,6 @@
   :after treemacs magit
   :ensure t)
 
-(use-package ranger
-  :ensure t
-  :bind* ("C-x C-d" . ranger))
 
 ;; dired config
 ;; ref - https://github.com/kaushalmodi/.emacs.d/blob/master/setup-files/setup-dired.el
