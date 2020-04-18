a98292d37bd9c0eb1e459a75c9fa1766474b28bb
Amol <amolgawai@users.noreply.github.com>
2020-04-19 00:35:39 +0200

    company-box and forge + magit enhancement
    Company box allows all the icons
    Forge is frontend for github and other git services
    Some magit enhancements
    Ref - https://github.com/TheBB/dotemacs/blob/master/init.el

--- config/commoncoding.el
+++ config/commoncoding.el
@@ -131,6 +131,31 @@ With a prefix argument, use comint-mode."
   (interactive)
   (kill-buffer)
   (jump-to-register :magit-fullscreen))
+  
+(use-package evil-magit
+  :after magit)
+
+;; github with magit
+(use-package forge
+  :after magit
+  :config
+  (evil-bind-key '(normal visual) magit-mode-map "," 'forge-dispatch)
+  (evil-bind-key 'normal magit-commit-section-map (kbd "gb") 'forge-browse-dwim)
+  (evil-bind-key 'normal magit-remote-section-map (kbd "gb") 'forge-browse-remote)
+  (evil-bind-key 'normal magit-branch-section-map (kbd "gb") 'forge-browse-branch)
+  (evil-bind-key 'normal forge-topic-mode-map (kbd "C-c C-c") 'forge-create-post))
+
+(use-package forge-list
+  :defer t
+  :config
+  (evil-bind-key 'normal forge-topic-list-mode-map (kbd "q") 'quit-window)
+  (evil-bind-key 'normal forge-topic-list-mode-map (kbd "o") 'forge-browse-topic))
+
+(use-package magit-todos
+  :after magit
+  :config
+  (magit-todos-mode))
+  
 ;; Ask password for pushing to remote
 (setenv "SSH_ASKPASS" "git-gui--askpass")
 
@@ -193,6 +218,54 @@ With a prefix argument, use comint-mode."
         company-tooltip-align-annotations t)
   (add-to-list 'company-backends 'company-jedi)
   (global-company-mode 1))
+
+;; icons for company mode
+;; ref - https://github.com/TheBB/dotemacs/blob/master/init.el
+(use-package company-box
+  :diminish company-box-mode
+  :hook (company-mode . company-box-mode)
+  :init
+  (setq company-box-icons-alist 'company-box-icons-all-the-icons)
+  :config
+  (require 'all-the-icons)
+  (setf (alist-get 'min-height company-box-frame-parameters) 6)
+  (setq company-box-icons-alist 'company-box-icons-all-the-icons
+        company-box-backends-colors nil
+
+        ;; These are the Doom Emacs defaults
+        company-box-icons-all-the-icons
+        `((Unknown       . ,(all-the-icons-material "find_in_page"             :face 'all-the-icons-purple))
+          (Text          . ,(all-the-icons-material "text_fields"              :face 'all-the-icons-green))
+          (Method        . ,(all-the-icons-material "functions"                :face 'all-the-icons-red))
+          (Function      . ,(all-the-icons-material "functions"                :face 'all-the-icons-red))
+          (Constructor   . ,(all-the-icons-material "functions"                :face 'all-the-icons-red))
+          (Field         . ,(all-the-icons-material "functions"                :face 'all-the-icons-red))
+          (Variable      . ,(all-the-icons-material "adjust"                   :face 'all-the-icons-blue))
+          (Class         . ,(all-the-icons-material "class"                    :face 'all-the-icons-red))
+          (Interface     . ,(all-the-icons-material "settings_input_component" :face 'all-the-icons-red))
+          (Module        . ,(all-the-icons-material "view_module"              :face 'all-the-icons-red))
+          (Property      . ,(all-the-icons-material "settings"                 :face 'all-the-icons-red))
+          (Unit          . ,(all-the-icons-material "straighten"               :face 'all-the-icons-red))
+          (Value         . ,(all-the-icons-material "filter_1"                 :face 'all-the-icons-red))
+          (Enum          . ,(all-the-icons-material "plus_one"                 :face 'all-the-icons-red))
+          (Keyword       . ,(all-the-icons-material "filter_center_focus"      :face 'all-the-icons-red))
+          (Snippet       . ,(all-the-icons-material "short_text"               :face 'all-the-icons-red))
+          (Color         . ,(all-the-icons-material "color_lens"               :face 'all-the-icons-red))
+          (File          . ,(all-the-icons-material "insert_drive_file"        :face 'all-the-icons-red))
+          (Reference     . ,(all-the-icons-material "collections_bookmark"     :face 'all-the-icons-red))
+          (Folder        . ,(all-the-icons-material "folder"                   :face 'all-the-icons-red))
+          (EnumMember    . ,(all-the-icons-material "people"                   :face 'all-the-icons-red))
+          (Constant      . ,(all-the-icons-material "pause_circle_filled"      :face 'all-the-icons-red))
+          (Struct        . ,(all-the-icons-material "streetview"               :face 'all-the-icons-red))
+          (Event         . ,(all-the-icons-material "event"                    :face 'all-the-icons-red))
+          (Operator      . ,(all-the-icons-material "control_point"            :face 'all-the-icons-red))
+          (TypeParameter . ,(all-the-icons-material "class"                    :face 'all-the-icons-red))
+          (Template      . ,(all-the-icons-material "short_text"               :face 'all-the-icons-green))))
+
+  ;; Add a space after the icon
+  (dolist (elt company-box-icons-all-the-icons)
+    (setcdr elt (concat (cdr elt) " "))))
+
 
 ;; Highlight todos, fixmes etc.
 (use-package hl-todo
